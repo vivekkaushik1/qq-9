5bcd123521e2f9d0602e1613bda5dfda7e08ffff
vivekkaushik1 <vivekit388@gmail.com>
2022-08-10 13:30:37 +0530

    ss

--- createCOmmit.sh
+++ createCOmmit.sh
@@ -1,11 +1,11 @@
 string=""
 x=1
 j=1
-while [ $x -le 200 ]
-do
- string="touch randomfile$((j+x)) && git add . && git commit -m '$((j+x)):randfile_$((j+x))'"
- echo $string
- eval "$string"
- x=$[$x+1]
+while [ $x -le 2 ]
+ do
+   git tag -a PerformanceTest11.$((j+x)) -m "vversion Pt1.1$((j+x))"
+   sh ./createCOmmitsTags.sh
+   x=$(( $x + 1 ))
+   echo "Completed Script Invocaton"
 done
-git push
+#git push
